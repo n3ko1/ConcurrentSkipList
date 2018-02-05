8477c1e28e00ca09e5af86c23b30f76ff69a54f0
d064754 <lucas.bader@sap.com>
2018-02-05 08:22:29 +0100

    skip list generic in value type

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,20 @@
 
 int main()
 {
-    SkipList l = SkipList();
+    SkipList<std::string> l = SkipList<std::string>();
     l.insert(1, "Hello");
     l.insert(2, "world.");
     l.insert(4, "Whatever.");
     l.print(std::cout);
     l.remove(2);
     l.print(std::cout);
+
+    SkipList<float> l2 = SkipList<float>();
+    l2.insert(1, 5.5);
+    l2.insert(2, 3.2);
+    l2.insert(4, 6);
+    l2.print(std::cout);
+    l2.remove(2);
+    l2.print(std::cout);
     return 0;
 }
--- src/skiplist.h
+++ src/skiplist.h
@@ -2,15 +2,19 @@
 #define SKIPLIST_H
 
 #include <string>
+#include <iostream>
+#include <limits>
+#include <random>
 
+template <typename ValueType>
 class SkipList
 {
 public:
   SkipList();
   ~SkipList(){};
 
-  std::string *search(const int searchKey) const;
-  void insert(const int key, const std::string &val);
+  ValueType *search(const int searchKey) const;
+  void insert(const int key, const ValueType &val);
   void remove(const int key);
 
   void print(std::ostream &os) const;
@@ -18,7 +22,11 @@ public:
 private:
   struct SkipNode
   {
-    SkipNode(const int k, const std::string &v, const int forward_size) : key(k), value(v)
+    SkipNode(const int k, const ValueType &v, const int forward_size) : key(k), value(v) { intialize_forward(forward_size); }
+    SkipNode(const int k, const int forward_size) : key(k) { intialize_forward(forward_size); }
+    ~SkipNode() { delete forward; }
+
+    void intialize_forward(const int forward_size)
     {
       forward = new SkipNode *[forward_size];
       for (auto i = 0; i != forward_size; ++i)
@@ -26,9 +34,9 @@ private:
         forward[i] = nullptr;
       }
     }
-    ~SkipNode() { delete forward; }
+
     int key;
-    std::string value;
+    ValueType value;
 
     // Array of forward nodes
     SkipNode **forward;
@@ -44,5 +52,139 @@ private:
   static const int max_levels = 16;
   const float probability;
 };
+
+template <typename ValueType>
+SkipList<ValueType>::SkipList() : probability(0.5)
+{
+  head = new SkipNode(std::numeric_limits<int>::min(), max_levels);
+  NIL = new SkipNode(std::numeric_limits<int>::max(), max_levels);
+  head->forward[0] = NIL;
+}
+
+template <typename ValueType>
+int SkipList<ValueType>::SkipNode::node_level() const
+{
+  int level;
+  for (level = 0; level != max_levels; ++level)
+  {
+    if (forward[level] == nullptr)
+      break;
+  }
+  return level;
+}
+
+template <typename ValueType>
+void SkipList<ValueType>::print(std::ostream &os) const
+{
+  auto x = head;
+  while (x->forward[0]->key != std::numeric_limits<int>::max())
+  {
+    x = x->forward[0]; // traverse to the right
+    os << "Key: " << x->key << " Value: " << x->value << " Level: " << x->node_level() << std::endl;
+  }
+}
+
+template <typename ValueType>
+ValueType *SkipList<ValueType>::search(const int searchKey) const
+{
+  auto x = head;
+  // traverse from top of head. Forward size of head is list level
+  for (int i = head->node_level() - 1; i >= 0; --i)
+  {
+    while (x->forward[i] && x->forward[i]->key < searchKey)
+    {
+      x = x->forward[i]; // traverse to the right
+    }
+  }
+
+  x = x->forward[0];
+  return (x->key == searchKey ? &x->value : nullptr);
+}
+
+template <typename ValueType>
+int SkipList<ValueType>::random_level() const
+{
+  int new_level = 1;
+
+  while (((double)rand() / (RAND_MAX)) < probability)
+  {
+    ++new_level;
+  }
+  return (new_level > max_levels ? max_levels : new_level);
+}
+
+template <typename ValueType>
+void SkipList<ValueType>::insert(const int key, const ValueType &val)
+{
+  auto x = head;
+  auto update = new SkipNode *[max_levels];
+
+  // traverse from top of head. Forward size of head is list level
+  for (int i = head->node_level() - 1; i >= 0; --i)
+  {
+    while (x->forward[i] && x->forward[i]->key < key)
+    {
+      x = x->forward[i]; // traverse to the right
+    }
+    update[i] = x; // store last forward pointer
+  }
+  if (x->forward[0]->key == key)
+  {
+    x->forward[0]->value = val; // update value
+  }
+  else
+  {
+    // insert new node
+    int new_level = random_level();
+    int list_level = head->node_level();
+
+    if (new_level > list_level)
+    {
+      for (auto i = list_level; i < new_level; ++i)
+      {
+        update[i] = head;
+      }
+    }
+    x = new SkipNode(key, val, max_levels);
+    for (auto i = 0; i < new_level; ++i)
+    {
+      x->forward[i] = update[i]->forward[i];
+      update[i]->forward[i] = x;
+    }
+  }
+
+  delete update;
+  return;
+}
+
+template <typename ValueType>
+void SkipList<ValueType>::remove(const int key)
+{
+  auto x = head;
+  auto update = new SkipNode *[max_levels];
+
+  // traverse from top of head. Forward size of head is list level
+  for (int i = head->node_level() - 1; i >= 0; --i)
+  {
+    while (x->forward[i] && x->forward[i]->key < key)
+    {
+      x = x->forward[i]; // traverse to the right
+    }
+    update[i] = x; // store last forward pointer
+  }
+  x = x->forward[0];
+  if (x->key == key)
+  {
+    for (int i = 0; i != head->node_level() - 1; ++i)
+    {
+      if (update[i]->forward[i] != x)
+        break;
+      update[i]->forward[i] = x->forward[i];
+    }
+    x = nullptr;
+  }
+  delete update;
+  return;
+}
 
 #endif
