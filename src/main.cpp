47abb0ae89a34d32ce753a4e88ba6608bfb8c30c
d064754 <lucas.bader@sap.com>
2018-02-20 11:06:46 +0100

    segfaulting when run outside of gdb

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,10 @@
 int main()
 {
     SkipList<int, std::string> l = SkipList<int, std::string>();
-    l.print(std::cout);
     l.insert(1, "Hello");
-    // l.insert(2, "world.");
-    // l.insert(4, "Whatever.");
-     l.print(std::cout);
+    l.insert(2, "world.");
+    l.insert(4, "Whatever.");
+    l.print(std::cout);
     // l.print(std::cout);
     return 0;
 }
